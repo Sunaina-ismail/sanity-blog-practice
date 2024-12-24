ae8184e2ac85e9716a8067aef12ed5bf9cac21ee
Sunaina-ismail <164198987+Sunaina-ismail@users.noreply.github.com>
2024-12-24 13:39:22 -0800

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import BlogItem from "@/components/Blog/Blog";
+import { getPosts } from "@/sanity/sanity-utils";
+
+export default async function Home() {
+  const posts = await getPosts();
+
+  return (
+    <div className="py-5">
+      {posts?.length > 0 ? (
+        posts.map((post: any, i) => <BlogItem key={i} blog={post} />)
+      ) : (
+        <p >No posts found</p>
+      )}
+ </div>
+);
+}
